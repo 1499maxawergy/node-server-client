7fa9a76468d91a5ce7eaeb4184e670a8f7b59d63
Maxim 'Maxawergy' Milko <maxawergy@yandex.ru>
2023-05-04 05:50:04 +0300

    AuctionWeb v1.1

--- client/src/App.js
+++ client/src/App.js
@@ -9,6 +9,7 @@ import React, {useContext, useEffect, useState} from "react";
 import {Context} from "./index";
 import {check} from "./http/userAPI";
 import {Spinner} from "react-bootstrap";
+import Footer from "./components/Footer";
 
 const App = observer(() => {
     const {user} = useContext(Context);
@@ -34,6 +35,7 @@ const App = observer(() => {
     <BrowserRouter>
         <NavBar/>
         <AppRouter/>
+        <Footer/>
     </BrowserRouter>
   );
 });
