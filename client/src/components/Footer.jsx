90392be6ebdace017d2a2d4886f7a66e6a147890
Maxim 'Maxawergy' Milko <maxawergy@yandex.ru>
2023-05-04 05:52:08 +0300

    AuctionWeb v1.1

---
+++ client/src/components/Footer.jsx
@@ -0,0 +1,16 @@
+import React from 'react';
+
+const Footer = () => {
+    return (
+        <footer className="bg-dark foot w-100">
+            <div className="container mb-auto">
+                <div className="">
+                    <p className="text-light">
+                        Copyright &copy; <a className={'text-light'} href={'https://github.com/1499maxawergy'}>Maxim Milko 2023</a>. All rights reserved</p>
+                </div>
+            </div>
+        </footer>
+    );
+};
+
+export default Footer;
